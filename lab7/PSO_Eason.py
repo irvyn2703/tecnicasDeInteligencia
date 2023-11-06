9bc7884ab05fce8a3f11ba97dd323a6b2966e68e
irvyn2703 <tosolatoso17@gmail.com>
2023-11-06 14:16:47 -0600

    practica PSO

---
+++ lab7/PSO_Eason.py
@@ -0,0 +1,97 @@
+import random
+import math
+import statistics
+
+minPoblacion = -100 # valor minimo de la poblacion
+maxPoblacion = 100 # valor maximo de la poblacion
+tamPoblacion = 10 # tamaño de la poblacion
+c1 = 0.35 
+c2 = 1.2
+dimProblema = 2
+w = 0.21
+maxIteracion = 100 # num de iteraciones
+r1 = random.uniform(0, 1) # numero aleatorio entre el 0 y 1
+r2 = random.uniform(0, 1) # numero aleatorio entre el 0 y 1
+velocidad = [[random.uniform(0, 1) for _ in range(2)] for _ in range(tamPoblacion)] # inicializamos una matriz aleatoria del tamaño de la poblacion
+position = [[random.uniform(minPoblacion, maxPoblacion) for _ in range(2)] for _ in range(tamPoblacion)] # inicializamos una matriz aleatoria del tamaño de la poblacion
+positionBest = position # mejores posiciones al inicio son las mismas que las posiociones
+mejorPosition = 0
+resultados = []
+logro = []
+fitnessPuntos = [[100, 100] for _ in range(tamPoblacion)] #[x][0] --> valor fitness de la iteracion [x][1] valor fitness de la mejor position
+
+# ------------------------------------------------- funciones -----------------------------------------
+def fitness(x,y):
+    return -math.cos(x) * math.cos(y) * math.exp(-(x - math.pi)**2 - (y - math.pi)**2) #eason
+    #return -(sum(i * math.cos((i + 1) * x + 1) for i in range(1, 6))) * (sum(i * math.cos((i + 1) * y + 1) for i in range(1, 6))) # shubert
+    #result = (1 - x)**2 + 100 * (y - x**2)**2
+    #return result
+
+
+def bestPosition():
+    best_value = fitnessPuntos[0][1]  # Suponemos que el primer valor es el mejor inicialmente
+    best_index = 0  # Índice del mejor valor
+
+    for i in range(0, tamPoblacion):
+        if (fitnessPuntos[i][1] < best_value):
+            best_value = fitnessPuntos[i][1]
+            best_index = i
+
+    return best_index
+
+def actualizacionVelocidad(velocidad,position,bestPosition,bestPunto):
+    nuevoValor = w*velocidad + c1*r1*(bestPosition - position) + c2*r2*(bestPunto - position)
+    return nuevoValor
+
+def actualizarPosition(position,velocidad):
+    temp = position + velocidad
+    if (temp <= maxPoblacion) and (temp >= minPoblacion):
+        return temp
+    else:
+        return position
+    
+def actualizarMejoresPuntos():
+    for i in range(0,tamPoblacion):
+        if(fitnessPuntos[i][0] < fitnessPuntos[i][1]):
+            print("cambio: " + str(positionBest[i][0]) + " " + str(positionBest[i][1]) + " a " + str(position[i][0]) + " " + str(position[i][1])) 
+            positionBest[i][0] = position[i][0]
+            positionBest[i][1] = position[i][1]
+            fitnessPuntos[i][1] = fitnessPuntos[i][0]
+
+# inicio del PSO
+for a in range(0,50):
+    velocidad = [[random.uniform(0, 1) for _ in range(2)] for _ in range(tamPoblacion)] # inicializamos una matriz aleatoria del tamaño de la poblacion
+    position = [[random.uniform(minPoblacion, maxPoblacion) for _ in range(2)] for _ in range(tamPoblacion)] # inicializamos una matriz aleatoria del tamaño de la poblacion
+    for z in range(1, maxIteracion + 1):
+        print("iteracion : " + str(z))
+        # calculamos el valor fitness y mostramos la poblacion
+        for i in range (0, tamPoblacion):
+            fitnessPuntos[i][0] = fitness(position[i][0], position[i][1])
+            print(str(velocidad[i][0]) + "," + str(velocidad[i][1]) + "  ||  " +str(position[i][0]) + "," + str(position[i][1]) + " = " + str(fitnessPuntos[i][0]))
+
+        # actualizamos los puntos
+        print("")
+        actualizarMejoresPuntos()
+
+        # obtenemos el mejor punto
+        print("")
+        mejorPosition = bestPosition()
+        print("mejor posicion = " + str(positionBest[mejorPosition][0]) + "," + str(positionBest[mejorPosition][1]) + " = " + str(fitnessPuntos[mejorPosition][1]))
+
+        #actualizamos velocidades y posiciones
+        for i in range(0, tamPoblacion):
+            velocidad[i][0] = actualizacionVelocidad(velocidad[i][0], position[i][0],positionBest[i][0],positionBest[mejorPosition][0])
+            velocidad[i][1] = actualizacionVelocidad(velocidad[i][1], position[i][1],positionBest[i][1],positionBest[mejorPosition][1])
+            position[i][0] = actualizarPosition(position[i][0], velocidad[i][0])
+            position[i][1] = actualizarPosition(position[i][1], velocidad[i][1])
+            if( fitnessPuntos[mejorPosition][1] == -1): # <-----------------
+                logro.append(z)
+        print("\n")
+    resultados.append(fitnessPuntos[mejorPosition][1])
+cont = 0
+for i in range(0,50):
+    #print(str(resultados[i])) # comentar esta linea <---------------------------------------
+    if(resultados[i] == -1): # cambiar< -------------------------------
+        cont = cont + 1
+
+print("exito: " + str(cont * 2) + "\% \npromedio:" + str(statistics.mean(logro)) + "\ndesviacion: " + str(statistics.stdev(logro)))
